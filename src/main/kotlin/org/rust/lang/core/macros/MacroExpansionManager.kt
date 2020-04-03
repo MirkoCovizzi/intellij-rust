b481c26f2dd6a474dff8486bc9a3a7f3dda618b2
vlad20012 <beskvlad@gmail.com>
2020-04-03 11:50:45 +0300

    MACRO: use common task queue with cargo sync
    This makes macro expansion cancellation more fast
    in the case of cargo refresh (e.g. Cargo.toml
    editing)
    
    Also:
    1. macro expansion is now cancelled immediately if
      the new engine is disabled in the settings;
    2. progress bar of macro expansion task is not
      shown until the end of the indexing

--- src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
+++ src/main/kotlin/org/rust/lang/core/macros/MacroExpansionManager.kt
@@ -13,9 +13,8 @@ import com.intellij.openapi.components.*
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.TextEditor
-import com.intellij.openapi.progress.*
-import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
-import com.intellij.openapi.progress.impl.ProgressManagerImpl
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentIterator
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx
@@ -23,8 +22,6 @@ import com.intellij.openapi.util.*
 import com.intellij.openapi.vfs.*
 import com.intellij.openapi.vfs.newvfs.RefreshQueue
 import com.intellij.openapiext.Testmark
-import com.intellij.openapiext.isDispatchThread
-import com.intellij.openapiext.isHeadlessEnvironment
 import com.intellij.openapiext.isUnitTestMode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -32,15 +29,13 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScopes
 import com.intellij.psi.util.CachedValueProvider
-import com.intellij.util.PairConsumer
-import com.intellij.util.concurrency.QueueProcessor
-import com.intellij.util.concurrency.QueueProcessor.ThreadToUse
 import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.IndexableFileSet
 import com.intellij.util.io.createDirectories
 import com.intellij.util.io.delete
 import com.intellij.util.io.exists
 import org.jetbrains.annotations.TestOnly
+import org.rust.RsTaskExt
 import org.rust.cargo.project.model.CargoProject
 import org.rust.cargo.project.model.CargoProjectsService
 import org.rust.cargo.project.model.cargoProjects
@@ -56,6 +51,7 @@ import org.rust.stdext.ThreadLocalDelegate
 import org.rust.stdext.newDeflaterDataOutputStream
 import org.rust.stdext.newInflaterDataInputStream
 import org.rust.stdext.randomLowercaseAlphabetic
+import org.rust.taskQueue
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -63,7 +59,6 @@ import java.util.concurrent.Executors
 import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.ForkJoinTask
 import java.util.concurrent.TimeUnit
-import java.util.function.BiConsumer
 
 interface MacroExpansionManager {
     val indexableDirectory: VirtualFile?
@@ -376,9 +371,6 @@ private class MacroExpansionServiceImplInner(
     private val storage: ExpandedMacroStorage,
     val expansionsDirVi: VirtualFile
 ) {
-
-    private val taskQueue = MacroExpansionTaskQueue(project)
-
     /**
      * We must use a separate pool because:
      * 1. [ForkJoinPool.commonPool] is heavily used by the platform
@@ -430,7 +422,7 @@ private class MacroExpansionServiceImplInner(
     private var performConsistencyCheckBeforeTask: Boolean = true
 
     private fun submitTask(task: Task.Backgroundable) {
-        taskQueue.run(task)
+        project.taskQueue.run(task)
     }
 
     @Synchronized
@@ -451,7 +443,7 @@ private class MacroExpansionServiceImplInner(
 
     private fun cleanMacrosDirectoryAndStorage() {
         performConsistencyCheckBeforeTask = false
-        submitTask(object : Task.Backgroundable(project, "Cleaning outdated macros", false) {
+        submitTask(object : Task.Backgroundable(project, "Cleaning outdated macros", false), RsTaskExt {
             override fun run(indicator: ProgressIndicator) {
                 checkReadAccessNotAllowed()
                 val vfs = MacroExpansionFileSystem.getInstance()
@@ -468,6 +460,9 @@ private class MacroExpansionServiceImplInner(
                     }
                 }
             }
+
+            override val taskType: RsTaskExt.TaskType
+                get() = RsTaskExt.TaskType.MACROS_CLEAR
         })
     }
 
@@ -568,14 +563,6 @@ private class MacroExpansionServiceImplInner(
         val treeChangeListener = ChangedMacroUpdater()
         PsiManager.getInstance(project).addPsiTreeChangeListener(treeChangeListener, disposable)
         ApplicationManager.getApplication().addApplicationListener(treeChangeListener, disposable)
-
-        if (isUnitTestMode) {
-            ApplicationManager.getApplication().addApplicationListener(object : ApplicationListener {
-                override fun afterWriteActionFinished(action: Any) {
-                    ensureUpToDate()
-                }
-            }, disposable)
-        }
 
         registerIndexableSet(disposable)
 
@@ -783,9 +770,7 @@ private class MacroExpansionServiceImplInner(
                 return storage.makeExpansionTask(calls)
             }
 
-            override fun canEat(other: MacroExpansionTaskBase): Boolean = true // eat everything
-
-            override val isProgressBarDelayed: Boolean get() = false
+            override val taskType: RsTaskExt.TaskType get() = RsTaskExt.TaskType.MACROS_UNPROCESSED
         }
         submitTask(ProcessUnprocessedMacrosTask())
     }
@@ -810,9 +795,10 @@ private class MacroExpansionServiceImplInner(
                 return runReadAction { storage.makeValidationTask(workspaceOnly) }
             }
 
-            override fun canEat(other: MacroExpansionTaskBase): Boolean {
-                return other is ProcessModifiedMacrosTask && (other.workspaceOnly || !workspaceOnly)
-            }
+            override val taskType: RsTaskExt.TaskType
+                get() = if (workspaceOnly) RsTaskExt.TaskType.MACROS_WORKSPACE else RsTaskExt.TaskType.MACROS_FULL
+
+            override val progressBarShowDelay: Int get() = 2000
         }
 
         val task = ProcessModifiedMacrosTask(workspaceOnly)
@@ -827,11 +813,6 @@ private class MacroExpansionServiceImplInner(
         }
 
         return false
-    }
-
-    private fun ensureUpToDate() {
-        check(isUnitTestMode)
-        taskQueue.ensureUpToDate()
     }
 
     fun getExpansionFor(call: RsMacroCall): CachedValueProvider.Result<MacroExpansion?> {
@@ -869,6 +850,29 @@ private class MacroExpansionServiceImplInner(
 
         setupListeners(disposable)
 
+        ApplicationManager.getApplication().addApplicationListener(object : ApplicationListener {
+            private var isProcessingUpdates = false
+
+            override fun afterWriteActionFinished(action: Any) {
+                awaitAllTasksFinish()
+            }
+
+            private fun awaitAllTasksFinish() {
+                check(isUnitTestMode)
+                checkWriteAccessNotAllowed()
+                val taskQueue = project.taskQueue
+                if (!taskQueue.isEmpty) {
+                    if (isProcessingUpdates) return
+                    isProcessingUpdates = true
+                    while (!taskQueue.isEmpty && !project.isDisposed) {
+                        LaterInvocator.dispatchPendingFlushes()
+                        Thread.sleep(10)
+                    }
+                    isProcessingUpdates = false
+                }
+            }
+        }, disposable)
+
         // TODO this causes flaky tests. Expanding should be triggered by an actual code change
         processUnprocessedMacros()
 
@@ -878,7 +882,7 @@ private class MacroExpansionServiceImplInner(
     private fun disposeUnitTest(saveCacheOnDispose: Boolean) {
         check(isUnitTestMode)
 
-        taskQueue.cancelAll()
+        project.taskQueue.cancelTasks(RsTaskExt.TaskType.MACROS_CLEAR)
 
         if (saveCacheOnDispose) {
             save()
@@ -902,143 +906,6 @@ class MacroExpansionManagerWaker : CargoProjectsService.CargoProjectsListener {
         if (projects.isNotEmpty()) {
             service.project.macroExpansionManager
         }
-    }
-}
-
-/** Inspired by [BackgroundTaskQueue] */
-private class MacroExpansionTaskQueue(val project: Project) {
-    private val processor = QueueProcessor<ContinuableRunnable>(
-        QueueConsumer(),
-        true,
-        ThreadToUse.AWT,
-        project.disposed
-    )
-
-    // Guarded by self object monitor (@Synchronized)
-    private val cancelableTasks: MutableList<BackgroundableTaskData> = mutableListOf()
-
-    @Synchronized
-    fun run(task: MacroExpansionTaskBase) {
-        cancelableTasks.removeIf {
-            if (it.task is MacroExpansionTaskBase && task.canEat(it.task)) {
-                it.cancel()
-                true
-            } else {
-                false
-            }
-        }
-        val data = BackgroundableTaskData(task, ::onFinish)
-        cancelableTasks += data
-        processor.add(data)
-    }
-
-    fun run(task: Task.Backgroundable) {
-        processor.add(BackgroundableTaskData(task) {})
-    }
-
-    fun runSimple(runnable: () -> Unit) {
-        processor.add(SimpleTaskData(runnable))
-    }
-
-    private var isProcessingUpdates = false
-
-    fun ensureUpToDate() {
-        check(isUnitTestMode)
-        if (isDispatchThread && !processor.isEmpty) {
-            checkWriteAccessNotAllowed()
-            if (isProcessingUpdates) return
-            isProcessingUpdates = true
-            while (!processor.isEmpty && !project.isDisposed) {
-                LaterInvocator.dispatchPendingFlushes()
-                Thread.sleep(10)
-            }
-            isProcessingUpdates = false
-        }
-    }
-
-    @Synchronized
-    fun cancelAll() {
-        for (task in cancelableTasks) {
-            task.cancel()
-        }
-        cancelableTasks.clear()
-    }
-
-    @Synchronized
-    private fun onFinish(data: BackgroundableTaskData) {
-        cancelableTasks.remove(data)
-    }
-
-    private interface ContinuableRunnable {
-        fun run(continuation: Runnable)
-    }
-
-    // BACKCOMPAT: 2019.3. get rid of [PairConsumer] implementation
-    private class QueueConsumer : PairConsumer<ContinuableRunnable, Runnable>, BiConsumer<ContinuableRunnable, Runnable> {
-        override fun consume(s: ContinuableRunnable, t: Runnable) = accept(s, t)
-        override fun accept(t: ContinuableRunnable, u: Runnable) = t.run(u)
-    }
-
-    private class BackgroundableTaskData(
-        val task: Task.Backgroundable,
-        val onFinish: (BackgroundableTaskData) -> Unit
-    ) : ContinuableRunnable {
-        private var state: State = State.Pending
-
-        @Synchronized
-        override fun run(continuation: Runnable) {
-            // BackgroundableProcessIndicator should be created from EDT
-            checkIsDispatchThread()
-            if (state != State.Pending) {
-                continuation.run()
-                return
-            }
-
-            val indicator = when {
-                isHeadlessEnvironment -> EmptyProgressIndicator()
-                task is MacroExpansionTaskBase && task.isProgressBarDelayed -> DelayedBackgroundableProcessIndicator(task, 2000)
-                else -> BackgroundableProcessIndicator(task)
-            }
-
-            state = State.Running(indicator)
-
-            val pm = ProgressManager.getInstance() as ProgressManagerImpl
-            pm.runProcessWithProgressAsynchronously(
-                task,
-                indicator,
-                {
-                    onFinish(this)
-                    continuation.run()
-                },
-                ModalityState.NON_MODAL
-            )
-        }
-
-        @Synchronized
-        fun cancel() {
-            when (val state = state) {
-                State.Pending -> this.state = State.Canceled
-                is State.Running -> state.indicator.cancel()
-                State.Canceled -> Unit
-            }
-        }
-
-        private sealed class State {
-            object Pending : State()
-            object Canceled : State()
-            data class Running(val indicator: ProgressIndicator) : State()
-        }
-    }
-
-    private class SimpleTaskData(val task: () -> Unit) : ContinuableRunnable {
-        override fun run(continuation: Runnable) {
-            try {
-                task()
-            } finally {
-                continuation.run()
-            }
-        }
-
     }
 }
 
